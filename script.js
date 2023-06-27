c4fec045cb8b35c526f0a8bdf86dc443a5fe441b
ShahriarCode <132796430+ShahriarCode69@users.noreply.github.com>
2023-06-27 13:02:43 +0600

    Add Passenger counter files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+
+
+//document.getElementById("count-el").innerHTML = 5;
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+
+
+let welcomeEl = document.getElementById("welcome-el")
+
+let greeting =  "Created By "
+let nameME = "©Shahriar"
+
+welcomeEl.innerText = greeting + nameME
+
+welcomeEl.innerText += "👋"
